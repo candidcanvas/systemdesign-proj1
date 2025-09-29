00d9aab8af4e76e50e16137797a87f9a0ef274b1
Breana Velasco <thecandidcanvas04@gmail.com>
2025-09-29 21:22:13 +0000

    added content to script.js and displays albums

---
+++ script.js
@@ -0,0 +1,41 @@
+const searchForm = document.querySelector('#searchForm');
+const mainContent = document.querySelector('#vinylShelf');
+
+// form submission event listener
+searchForm.addEventListener("submit", event => {
+    event.preventDefault();
+
+    let searchText = document.querySelector('#searchText').value;
+
+    let url = new URL('https://corsproxy.io/?url=https://api.deezer.com/search/album');
+    url.searchParams.set('q', searchText);
+    url.searchParams.set('limit', 10);
+
+    console.log(url.href);
+
+    // run request
+    fetch(url.href)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            displayResults(response.data);
+        })
+        .catch(err => console.error(err));
+});
+
+// display results
+const displayResults = (results) => {
+    mainContent.innerHTML = '';
+    if (results) {
+        results.forEach(result => {
+            let div = document.createElement('div');
+            div.classList.add('result');
+
+            div.innerHTML =
+                '<img src="' + result.cover_medium + '" alt="' + result.title + '"/>'
+            mainContent.appendChild(div);
+        })
+    } else {
+        mainContent.innerHTML = '<p>No results found</p>';
+    }
+}
