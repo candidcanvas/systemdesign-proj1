e0299182350a1c3c66742291b941f7e54ca49eb9
Breana Velasco <thecandidcanvas04@gmail.com>
2025-10-02 16:11:57 +0000

    turned images into buttons and added receipt.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// import { createProfile } from './album.js';
+// import { createProfile } from './receipt.js';
 
-const searchForm = document.querySelector('#searchForm');
-const mainContent = document.querySelector('#vinylShelf');
+const searchForm = document.querySelector(`#searchForm`);
+const mainContent = document.querySelector(`#vinylShelf`);
 
 // form submission event listener
 searchForm.addEventListener("submit", event => {
     event.preventDefault();
 
-    let searchText = document.querySelector('#searchText').value;
+    let searchText = document.querySelector(`#searchText`).value;
 
-    let url = new URL('https://corsproxy.io/?url=https://api.deezer.com/search/album');
-    url.searchParams.set('q', searchText);
-    url.searchParams.set('limit', 10);
+    let url = new URL(`https://corsproxy.io/?url=https://api.deezer.com/search/album`);
+    url.searchParams.set(`q`, searchText);
+    url.searchParams.set(`limit`, 10);
 
     console.log(url.href);
 
@@ -30,35 +30,28 @@ const displayResults = (results) => {
     mainContent.innerHTML = '';
     if (results) {
         results.forEach(result => {
+            // const popoverId = `popover-${result.id}`;
+
             let div = document.createElement('div');
             div.classList.add('result');
 
-            div.innerHTML =
-                '<img src="' + result.cover_medium + '" alt="' + result.title + '" style="cursor:pointer" width="200px"/>';
+            let template =
+                `<button class="open" popoverTarget="${result.id}" >
+                    <img src="${result.cover_medium}" alt="${result.title}" style="cursor:pointer" width="200px"/>
+                    <div class="shelf"></div>
+                </button>
 
-            // click event for popover
-            div.querySelector('img').addEventListener('click', async (e) => {
-                // remove existing popovers
-                const oldPopovers = document.querySelector('.popover');
-                if (oldPopover) oldPopovers.remove();
-
-                // fetch tracklist details
-                const albumUrl = 'https://corsproxy.io/?url=https://api.deezer.com/album/${result.id}/tracks';
-                let tracks = [];
-                try {
-                    const res = await fetch(albumUrl);
-                    const data = await res.json();
-                    tracks = data.tracks?.data || [];
-                } catch (err) {
-                    tracks = [];
-                }
-
-                // create popover
-                
-            });
+                <div popover id="${result.id}">
+                    <div class="profile">
+                        <p>Loading...</p>
+                    </div>
+                </div>`;
+            div.innerHTML = template;
+                            
             mainContent.appendChild(div);
-        })
+        });
+
     } else {
-        mainContent.innerHTML = '<p>No results found</p>';
+        mainContent.innerHTML = `<p>No results found</p>`;
     }
 }
